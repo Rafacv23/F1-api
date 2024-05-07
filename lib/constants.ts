31b4d3c26d621c38c81bbe7e0a69940c17450187
Rafa Canosa <rafacv23@gmail.com>
2024-05-07 18:29:39 +0200

    added constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,2 +1,6 @@
-export const SITE_URL = ""
-export const SITE_NAME = ""
+export const SITE_URL = "https://f1-connect-api.vercel.app/"
+export const SITE_NAME = "F1 Connect"
+export const SITE_META_NAME =
+  "F1 Connect API | Your free API ready for development"
+export const SITE_META_DESCRIPTION =
+  "F1 Connect API it's a free API that provides data about all Formula 1 Seasons."
