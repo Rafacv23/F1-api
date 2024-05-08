b4e33841ec6cb2b27c9e4589f9413e2b609a58ec
Rafa Canosa <rafacv23@gmail.com>
2024-05-08 15:10:25 +0200

    new processed data

--- app/api/[year]/[round]/route.ts
+++ app/api/[year]/[round]/route.ts
@@ -1,12 +1,14 @@
 import { NextResponse } from "next/server"
 import { client } from "@/app/lib/turso.js"
+import { SITE_URL } from "@/lib/constants"
 
 export async function GET(request: Request, context: any) {
   const { year, round } = context.params
   const limit = 1
-  const sql = `SELECT Races.*
+  const sql = `SELECT Races.*, Circuits.*
   FROM Races
   JOIN Championships ON Races.Championship_ID = Championships.Championship_ID
+  JOIN Circuits ON Races.Circuit = Circuits.Circuit_ID
   WHERE Championships.Year = ? AND Races.Round = ?
   LIMIT ?;
   `
@@ -16,8 +18,6 @@ export async function GET(request: Request, context: any) {
     args: [year, round, limit],
   })
 
-  console.log(data)
-
   // Procesamos los datos
   const processedData = data.rows.map((row) => {
     return {
@@ -25,7 +25,21 @@ export async function GET(request: Request, context: any) {
       Championship_ID: row[1],
       Race_Name: row[2],
       Race_Date: row[3],
-      Circuit: row[4],
+      Circuit: {
+        // Circuit info
+        circuitId: row[10],
+        circuitName: row[11],
+        country: row[12],
+        city: row[13],
+        circuitLength: row[14] + "km",
+        lapRecord: row[15],
+        firstParticipationYear: row[16],
+        numberOfCorners: row[17],
+        fastestLapDriverId: row[18],
+        fastestLapTeamId: row[19],
+        fastestLapYear: row[20],
+        url: row[21],
+      },
       Laps: row[5],
       Winner_ID: row[6],
       Team_Winner_ID: row[7],
@@ -35,6 +49,12 @@ export async function GET(request: Request, context: any) {
   })
 
   return NextResponse.json({
+    api: SITE_URL,
+    url: request.url,
+    limit: limit,
+    total: processedData.length,
+    season: year,
+    round: round,
     race: processedData,
   })
 }
