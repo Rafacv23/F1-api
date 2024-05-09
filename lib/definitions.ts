68e55d119929054f4e38852e69b1010ecd49a71d
Rafa Canosa <rafacv23@gmail.com>
2024-05-09 12:08:11 +0200

    definitions for ts

---
+++ lib/definitions.ts
@@ -0,0 +1,15 @@
+export type EndpointType = {
+  url: string
+  title: string
+  description: string
+}
+
+export type FaqType = {
+  question: string
+  answer: string
+}
+
+export type TermType = {
+  title: string
+  description: string
+}
