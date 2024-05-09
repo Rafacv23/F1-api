7e9ef8d8780042d89c01fd6b16e472787e732702
Rafa Canosa <rafacv23@gmail.com>
2024-05-09 14:51:05 +0200

    remove ul, add table

--- components/EndpointsList.tsx
+++ components/EndpointsList.tsx
@@ -1,20 +1,42 @@
 import React from "react"
-import Endpoint from "@/components/ui/Endpoint"
 import endpoints from "@/app/data/endpoints.json"
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
 export default function EndpointsList() {
   return (
     <>
       <h2 className="text-2xl font-bold mb-2">Endpoints</h2>
-      <ul className="list-disc pl-6">
-        {endpoints.map((endpoint) => (
-          <Endpoint
-            key={endpoint.title}
-            url={endpoint.url}
-            title={endpoint.title}
-            description={endpoint.description}
-          />
-        ))}
-      </ul>
+      <Table>
+        <TableCaption>A list of all the API endpoints.</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Endpoint</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Example</TableHead>
+            <TableHead className="text-right">Params</TableHead>
+            <TableHead className="text-right">Default</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {endpoints.map((endpoint) => (
+            <TableRow key={endpoint.url}>
+              <TableCell className="font-medium">{endpoint.title}</TableCell>
+              <TableCell>{endpoint.description}</TableCell>
+              <TableCell>{endpoint.url}</TableCell>
+              <TableCell>{endpoint.params}</TableCell>
+              <TableCell>{endpoint.default}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
     </>
   )
 }
