f28cf1408dfd6f082c617354daff587faf75cf84
Rafa Canosa <rafacv23@gmail.com>
2024-05-09 12:07:48 +0200

    remove unnececesary variable

--- components/EndpointsList.tsx
+++ components/EndpointsList.tsx
@@ -2,12 +2,11 @@ import React from "react"
 import Endpoint from "@/components/ui/Endpoint"
 import endpoints from "@/app/data/endpoints.json"
 export default function EndpointsList() {
-  const data = endpoints
   return (
     <>
       <h2 className="text-2xl font-bold mb-2">Endpoints</h2>
       <ul className="list-disc pl-6">
-        {data.endpoints.map((endpoint) => (
+        {endpoints.map((endpoint) => (
           <Endpoint
             key={endpoint.title}
             url={endpoint.url}
--- components/FaqsList.tsx
+++ components/FaqsList.tsx
@@ -3,11 +3,9 @@ import faqs from "@/app/data/faqs.json"
 import Faq from "./ui/Faq"
 
 export default async function FaqsList() {
-  const data = faqs
-
   return (
     <ul className="flex flex-col gap-4">
-      {data.faqs.map((faq) => (
+      {faqs.map((faq) => (
         <Faq key={faq.question} question={faq.question} answer={faq.answer} />
       ))}
     </ul>
