ec8fdd66aad0067ab9162075e09ec353b7bcc2ac
Rafa Canosa <rafacv23@gmail.com>
2024-05-09 13:27:58 +0200

    error returned when api not found any results

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,16 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// return error message when the api not found any result.
+
+import { NextResponse } from "next/server"
+import { SITE_URL } from "@/lib/constants"
+
+export function apiNotFound(request: Request, message: string) {
+  return NextResponse.json({
+    api: SITE_URL,
+    url: request.url,
+    message: message,
+  })
+}
