26b95e4eab5243449493e524dd06cffe6ba96f6c
Rafa Canosa <rafacv23@gmail.com>
2024-05-09 16:31:59 +0200

    styles for the logo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import HomeBanner from "@/components/HomeBanner"
 export default function Page() {
   return (
     <main className="max-w-3xl mx-auto p-6 h-screen flex flex-col justify-center items-center">
-      <img src="/logo.avif" width={200} />
       <HomeBanner />
     </main>
   )
