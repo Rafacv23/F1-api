50a4014c2a75ce248cd7eff4a671eb788dd5a290
Rafa Canosa <rafacv23@gmail.com>
2024-05-23 12:23:29 +0200

    add seo tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,22 @@
 import type { Metadata } from "next"
-import { SITE_META_NAME, SITE_META_DESCRIPTION } from "@/lib/constants"
+import {
+  SITE_META_NAME,
+  SITE_META_DESCRIPTION,
+  SITE_URL,
+} from "@/lib/constants"
 import "./globals.css"
 import Footer from "@/components/Footer"
 
 export const metadata: Metadata = {
   title: SITE_META_NAME,
   description: SITE_META_DESCRIPTION,
+  creator: "Rafa Canosa",
+  metadataBase: new URL(SITE_URL),
+  alternates: {
+    canonical: "/",
+  },
+  keywords: ["F1", "Public API", "Formula 1"],
+  referrer: "origin-when-cross-origin",
 }
 
 export default function RootLayout({
