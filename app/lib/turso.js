7b58e81729daf06e774853b8d70170f6e9044090
Rafa Canosa <rafacv23@gmail.com>
2024-05-27 17:01:53 +0200

    sync interval

--- app/lib/turso.js
+++ app/lib/turso.js
@@ -3,4 +3,5 @@ import { createClient } from "@libsql/client"
 export const client = createClient({
   url: process.env.TURSO_DATABASE_URL,
   authToken: process.env.TURSO_AUTH_TOKEN,
+  syncInterval: 60,
 })
