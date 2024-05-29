29e639a556c9be87cd15d1444d0e4832dbdf5215
Rafa Canosa <rafacv23@gmail.com>
2024-05-29 22:13:35 +0200

    ordered championships

--- app/demo/seasons/page.tsx
+++ app/demo/seasons/page.tsx
@@ -17,6 +17,10 @@ export default async function Seasons() {
 
   const data = await res.json()
 
+  const sortedChampionships = data.championships.sort(
+    (a: Championship, b: Championship) => b.year - a.year
+  )
+
   return (
     <Table>
       <TableCaption>A list of all F1 Championships.</TableCaption>
@@ -27,33 +31,29 @@ export default async function Seasons() {
           <TableHead>Wiki</TableHead>
         </TableRow>
       </TableHeader>
-      {data
-        ? data.championships.map((championship: Championship) => (
-            <>
-              <TableBody>
-                <TableRow key={championship.championshipId}>
-                  <TableCell className="font-medium">
-                    <a
-                      href={`/demo/${championship.year}`}
-                      title={championship.championshipName}
-                    >{`${championship.championshipName}`}</a>
-                  </TableCell>
-                  <TableCell className="font-medium">
-                    {`${championship.year}`}
-                  </TableCell>
-                  <TableCell>
-                    <a
-                      href={championship.url}
-                      title={championship.url}
-                      target="_blank"
-                      rel="noreferrer"
-                    >{`wiki`}</a>
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </>
-          ))
-        : null}
+      <TableBody>
+        {sortedChampionships.map((championship: Championship) => (
+          <TableRow key={championship.championshipId}>
+            <TableCell className="font-medium">
+              <a
+                href={`/demo/${championship.year}`}
+                title={championship.championshipName}
+              >{`${championship.championshipName}`}</a>
+            </TableCell>
+            <TableCell className="font-medium">
+              {`${championship.year}`}
+            </TableCell>
+            <TableCell>
+              <a
+                href={championship.url}
+                title={championship.url}
+                target="_blank"
+                rel="noreferrer"
+              >{`wiki`}</a>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
     </Table>
   )
 }
