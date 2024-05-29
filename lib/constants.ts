39adb77ff7e22fe3a6ed46ba52d59d5a9b1d08e2
Rafa Canosa <rafacv23@gmail.com>
2024-05-30 00:00:51 +0200

    new url

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
-export const SITE_URL = "https://f1-connect-api.vercel.app/"
-export const SITE_URL_API = "https://f1-connect-api.vercel.app/api/"
+export const SITE_URL = "https://f1connectapi.vercel.app/"
+export const SITE_URL_API = "https://f1connectapi.vercel.app/api/"
 export const SITE_NAME = "F1 Connect"
 export const SITE_META_NAME =
   "F1 Connect API | Your free API ready for development"
