d7341b3f9abde1c64a69d1109ab2b5e8be30a5b2
Rafa Canosa <rafacv23@gmail.com>
2024-06-03 14:59:59 +0200

    new routes for taking the last free practices, and qualy results (current version)

---
+++ app/api/current/last/fp1/route.tsx
@@ -0,0 +1,106 @@
+import { NextResponse } from "next/server"
+import { SITE_URL } from "@/lib/constants"
+import { executeQuery } from "@/lib/executeQuery"
+import { apiNotFound, getDay, getYear } from "@/lib/utils"
+
+//revalidate
+
+export const revalidate = 60
+
+export async function GET(request: Request) {
+  const queryParams = new URL(request.url).searchParams
+  const limit = queryParams.get("limit") || 30
+  try {
+    const year = getYear()
+    const today = getDay()
+
+    const sql = `
+      SELECT FP1.*, Races.*, Drivers.*, Teams.*, Circuits.*
+      FROM FP1
+      JOIN Races ON FP1.Race_ID = Races.Race_ID
+      JOIN Championships ON Races.Championship_ID = Championships.Championship_ID
+      JOIN Drivers ON FP1.Driver_ID = Drivers.Driver_ID
+      JOIN Teams ON FP1.Team_ID = Teams.Team_ID
+      JOIN Circuits ON Races.Circuit = Circuits.Circuit_ID
+      WHERE Championships.Year = ? AND Races.Race_Date <= ?
+      ORDER BY Races.Race_Date DESC, FP1.Time ASC
+      LIMIT ?;
+    `
+
+    const data = await executeQuery(sql, [year, today, limit])
+
+    if (data.length === 0) {
+      return apiNotFound(
+        request,
+        "No qualy results found for this round. Try with other one."
+      )
+    }
+
+    // Procesamos los datos
+    const processedData = data.map((row) => ({
+      fp1Id: row[0],
+      raceId: row[1],
+      driverId: row[2],
+      teamId: row[3],
+      time: row[4],
+      driver: {
+        driverId: row[2],
+        name: row[29],
+        surname: row[30],
+        country: row[31],
+        birthday: row[32],
+        number: row[33],
+        shortName: row[34],
+        url: row[35],
+      },
+      team: {
+        teamId: row[36],
+        teamName: row[37],
+        nationality: row[38],
+        firstAppareance: row[39],
+        constructorsChampionships: row[40],
+        driversChampionships: row[41],
+        url: row[42],
+      },
+    }))
+
+    // Obtener el circuito correspondiente a la carrera
+    const circuitData = {
+      circuitId: data[0][43],
+      circuitName: data[0][44],
+      country: data[0][45],
+      city: data[0][46],
+      circuitLength: data[0][47] + "km",
+      lapRecord: data[0][48],
+      firstParticipationYear: data[0][49],
+      corners: data[0][50],
+      fastestLapDriverId: data[0][51],
+      fastestLapTeamId: data[0][52],
+      fastestLapYear: data[0][53],
+      url: data[0][54],
+    }
+
+    return NextResponse.json({
+      api: SITE_URL,
+      url: request.url,
+      limit: limit,
+      total: data.length,
+      RaceTable: {
+        season: year,
+        date: data[0][17],
+        time: data[0][23],
+        Races: [
+          {
+            url: data[0][13],
+            raceName: data[0][7],
+            Circuit: circuitData,
+            FP1_Results: processedData,
+          },
+        ],
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.error()
+  }
+}
---
+++ app/api/current/last/qualy/route.tsx
@@ -0,0 +1,108 @@
+import { NextResponse } from "next/server"
+import { SITE_URL } from "@/lib/constants"
+import { executeQuery } from "@/lib/executeQuery"
+import { apiNotFound, getDay, getYear } from "@/lib/utils"
+
+//revalidate
+
+export const revalidate = 60
+
+export async function GET(request: Request) {
+  const queryParams = new URL(request.url).searchParams
+  const limit = queryParams.get("limit") || 20
+  try {
+    const year = getYear()
+    const today = getDay()
+    const sql = `
+      SELECT Classifications.*, Races.*, Drivers.*, Teams.*, Circuits.*
+      FROM Classifications
+      JOIN Races ON Classifications.Race_ID = Races.Race_ID
+      JOIN Championships ON Races.Championship_ID = Championships.Championship_ID
+      JOIN Drivers ON Classifications.Driver_ID = Drivers.Driver_ID
+      JOIN Teams ON Classifications.Team_ID = Teams.Team_ID
+      JOIN Circuits ON Races.Circuit = Circuits.Circuit_ID
+      WHERE Championships.Year = ? AND Races.Race_Date <= ?
+      ORDER BY Races.Race_Date DESC, Classifications.Grid_Position ASC
+      LIMIT ?;
+    `
+
+    const data = await executeQuery(sql, [year, today, limit])
+
+    if (data.length === 0) {
+      return apiNotFound(
+        request,
+        "No qualy results found for this round. Try with other one."
+      )
+    }
+
+    // Procesamos los datos
+    const processedData = data.map((row) => ({
+      Qualification_ID: row[0],
+      Race_ID: row[1],
+      Driver_ID: row[2],
+      Team_ID: row[3],
+      Q1_Time: row[4],
+      Q2_Time: row[5],
+      Q3_Time: row[6],
+      Grid_Position: row[7],
+      driver: {
+        driverId: row[2],
+        name: row[32],
+        surname: row[33],
+        country: row[34],
+        birthday: row[35],
+        number: row[36],
+        shortName: row[37],
+        url: row[38],
+      },
+      team: {
+        teamId: row[39],
+        teamName: row[40],
+        nationality: row[41],
+        firstAppareance: row[42],
+        constructorsChampionships: row[43],
+        driversChampionships: row[44],
+        url: row[45],
+      },
+    }))
+
+    // Obtener el circuito correspondiente a la carrera
+    const circuitData = {
+      circuitId: data[0][46],
+      circuitName: data[0][47],
+      country: data[0][48],
+      city: data[0][49],
+      circuitLength: data[0][50] + "km",
+      lapRecord: data[0][51],
+      firstParticipationYear: data[0][52],
+      corners: data[0][53],
+      fastestLapDriverId: data[0][54],
+      fastestLapTeamId: data[0][55],
+      fastestLapYear: data[0][56],
+      url: data[0][57],
+    }
+
+    return NextResponse.json({
+      api: SITE_URL,
+      url: request.url,
+      limit: limit,
+      total: data.length,
+      RaceTable: {
+        season: year,
+        date: data[0][19],
+        time: data[0][25],
+        Races: [
+          {
+            url: data[0][16],
+            raceName: data[0][10],
+            Circuit: circuitData,
+            Results: processedData,
+          },
+        ],
+      },
+    })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.error()
+  }
+}
