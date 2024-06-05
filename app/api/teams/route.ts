5113b8a42f4cef089e42e731cd44478afd963fc1
Rafa Canosa <rafacv23@gmail.com>
2024-06-05 10:23:52 +0200

    added interfaces

--- app/api/teams/route.ts
+++ app/api/teams/route.ts
@@ -2,40 +2,51 @@ import { NextResponse } from "next/server"
 import { SITE_URL } from "@/lib/constants"
 import { executeQuery } from "@/lib/executeQuery"
 import { apiNotFound } from "@/lib/utils"
+import { ProcessedTeam, Team, Teams } from "@/lib/definitions"
 
 export const revalidate = 60
+
+interface ApiResponse {
+  api: string
+  url: string
+  limit: string | number
+  total: number
+  teams: ProcessedTeam[]
+}
 
 export async function GET(request: Request) {
   const queryParams = new URL(request.url).searchParams
   const limit = queryParams.get("limit") || 30
   try {
     const sql = "SELECT * FROM teams LIMIT ?;"
-    const data = await executeQuery(sql, [limit])
+    const data: Teams = await executeQuery(sql, [limit])
 
     // Verificar si se encontraron datos
     if (data.length === 0) {
       return apiNotFound(request, "No teams found.")
     }
     // Procesamos los datos
-    const processedData = data.map((row) => {
+    const processedData = data.map((row: Team) => {
       return {
-        teamId: row[0],
-        teamName: row[1],
-        country: row[2],
-        firstAppareance: row[3],
-        constructorsChampionships: row[4],
-        driversChampionships: row[5],
-        url: row[6],
+        teamId: row.Team_ID,
+        teamName: row.Team_Name,
+        country: row.Team_Nationality,
+        firstAppareance: row.First_Appareance,
+        constructorsChampionships: row.Constructors_Championships,
+        driversChampionships: row.Drivers_Championships,
+        url: row.URL,
       }
     })
 
-    return NextResponse.json({
+    const response: ApiResponse = {
       api: SITE_URL,
       url: request.url,
       limit: limit,
       total: processedData.length,
       teams: processedData,
-    })
+    }
+
+    return NextResponse.json(response)
   } catch (error) {
     console.error("Error:", error) // Agregamos un mensaje de error para la consola
     return NextResponse.error()
