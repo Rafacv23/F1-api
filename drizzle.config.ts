51de36420af763ba3c63ac889bd213e01a848e79
Rafa <rafacv23@gmail.com>
2024-12-10 13:31:41 +0100

    feat: create drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,13 @@
+require("dotenv").config()
+
+import type { Config } from "drizzle-kit"
+
+export default {
+  schema: "./db/schema.ts",
+  out: "./db/migrations",
+  dialect: "turso",
+  dbCredentials: {
+    url: process.env.TURSO_DATABASE_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
+} satisfies Config
