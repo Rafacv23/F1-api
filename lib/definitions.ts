ac65284e26e753176af39e63f9c08cb429e8f246
Rafa <rafacv23@gmail.com>
2024-12-10 13:47:24 +0100

    update: add offset to apiResponse interface

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -183,6 +183,7 @@ export type ProcessedChampionships = ProcessedChampionship[]
 export interface BaseApiResponse {
   api: string
   url: string
+  offset?: string | number
   limit: string | number
   total?: number
 }
