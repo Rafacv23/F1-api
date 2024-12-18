ea0a272ffd6c4ab63df7e36bdcec143b30390a78
Rafa <rafacv23@gmail.com>
2024-12-18 12:26:18 +0100

    update: new meta description

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,6 +3,6 @@ export const SITE_URL_API = "https://f1connectapi.vercel.app/api/"
 export const SITE_NAME = "F1 Connect"
 export const SITE_TITLE = "F1 Connect Api."
 export const SITE_META_NAME =
-  "F1 Connect API | Your free API ready for development"
+  "F1 Connect API | Free and open source F1 API that provides data about all Formula 1 Seasons."
 export const SITE_META_DESCRIPTION =
-  "F1 Connect API it's a free API that provides data about all Formula 1 Seasons."
+  "F1 Connect API it's a free and open source API that provides data about all Formula 1 Seasons. The API offers realtime data and historical."
