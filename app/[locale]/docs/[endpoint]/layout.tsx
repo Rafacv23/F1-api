c1ac3a3dc49338b681acc18de0113141d6a55531
Rafa <rafacv23@gmail.com>
2024-12-27 12:46:20 +0100

    feat: replace manual navigation with DocsNav component in layout

--- app/[locale]/docs/[endpoint]/layout.tsx
+++ app/[locale]/docs/[endpoint]/layout.tsx
@@ -1,11 +1,4 @@
-import Link from "next/link"
-import endpoints from "@/content/endpoints.json"
-import {
-  Accordion,
-  AccordionContent,
-  AccordionItem,
-  AccordionTrigger,
-} from "@/components/ui/accordion"
+import DocsNav from "@/components/DocsNav"
 
 export default function DocsLayout({
   children,
@@ -18,36 +11,7 @@ export default function DocsLayout({
 }>) {
   return (
     <div className="flex p-6 mt-32 max-w-5xl w-full justify-center mx-auto">
-      <nav className="hidden md:block w-full md:w-1/6 border-r pr-4 border-headerBorder dark:border-customGrayDark sticky top-32 h-screen overflow-y-auto">
-        <h3 className="font-semibold text-lg mb-4">Endpoints</h3>
-        <ul>
-          {endpoints.map((endpoint) => (
-            <li key={endpoint.id}>
-              <Accordion type="single" collapsible>
-                <AccordionItem value="item-1">
-                  <AccordionTrigger>
-                    <Link href={`/docs/${endpoint.id}`}>{endpoint.title}</Link>
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    <ul>
-                      {endpoint.endpoints.map((subEndpoint) => (
-                        <li key={subEndpoint.id}>
-                          <Link
-                            href={`/docs/${endpoint.id}/${subEndpoint.id}`}
-                            className="text-slate-500 hover:text-black dark:text-slate-300 dark:hover:text-white hover:transition-all ease-in-out duration-300"
-                          >
-                            {subEndpoint.title}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </li>
-          ))}
-        </ul>
-      </nav>
+      <DocsNav />
       {children}
     </div>
   )
