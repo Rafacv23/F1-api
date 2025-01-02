55a110978382982b1d945fdb27d5dbeb5c0c9dd7
Rafa <rafacv23@gmail.com>
2025-01-02 17:15:24 +0100

    test: add API tests for constructors, drivers, and teams championship endpoints

---
+++ tests/api/[year]/[round]/race.spec.ts
@@ -0,0 +1,97 @@
+import { test, expect } from "@playwright/test"
+
+test.describe("GET /api/[year]/[round]/race", async () => {
+  test("should return race data for a valid year and round, with default limit and offset", async ({
+    request,
+  }) => {
+    const response = await request.get(`/api/2024/3/race`)
+    expect(response.ok()).toBeTruthy()
+    expect(response.status()).toBe(200)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      limit: expect.any(Number),
+      offset: expect.any(Number),
+      total: expect.any(Number),
+      season: expect.any(String),
+      races: expect.any(Object),
+    })
+
+    // Verificar una carrera dentro de la respuesta
+    if (data.races.length > 0) {
+      const race = data.races[0]
+      expect(race).toMatchObject({
+        round: expect.any(Number),
+        date: expect.any(String),
+        time: expect.any(String),
+        url: expect.any(String),
+        raceId: expect.any(String),
+        raceName: expect.any(String),
+        circuit: expect.any(Object),
+        results: expect.any(Array),
+      })
+    }
+  })
+
+  test("should return race data for a valid year and round, with limit and offset", async ({
+    request,
+  }) => {
+    const response = await request.get(`/api/2024/3/race?limit=1&offset=1`)
+    expect(response.ok()).toBeTruthy()
+    expect(response.status()).toBe(200)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      limit: expect.any(Number),
+      offset: expect.any(Number),
+      total: expect.any(Number),
+      season: expect.any(String),
+      races: expect.any(Object),
+    })
+
+    // Verificar una carrera dentro de la respuesta
+    if (data.races.length > 0) {
+      const race = data.races[0]
+      expect(race).toMatchObject({
+        round: expect.any(Number),
+        date: expect.any(String),
+        time: expect.any(String),
+        url: expect.any(String),
+        raceId: expect.any(String),
+        raceName: expect.any(String),
+        circuit: expect.any(Object),
+        results: expect.any(Array),
+      })
+    }
+  })
+
+  test("should return 404 when no year are found", async ({ request }) => {
+    const response = await request.get(`/api/2099/1/race`)
+    expect(response.status()).toBe(404)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      message: expect.any(String),
+      status: 404,
+    })
+  })
+
+  test("should return 404 when no round are found", async ({ request }) => {
+    const response = await request.get(`/api/2024/30/race`)
+    expect(response.status()).toBe(404)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      message: expect.any(String),
+      status: 404,
+    })
+  })
+})
---
+++ tests/api/[year]/[round]/sprint/qualy.spec.ts
@@ -0,0 +1,114 @@
+import { test, expect } from "@playwright/test"
+
+test.describe("GET /api/[year]/[round]/sprint/qualy", async () => {
+  test("should return sprint qualy race data for a valid year and round, with default limit and offset", async ({
+    request,
+  }) => {
+    const response = await request.get(`/api/2024/5/sprint/qualy`)
+    expect(response.ok()).toBeTruthy()
+    expect(response.status()).toBe(200)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      limit: expect.any(Number),
+      offset: expect.any(Number),
+      total: expect.any(Number),
+      season: expect.any(String),
+      races: expect.any(Object),
+    })
+
+    // Verificar una carrera dentro de la respuesta
+    if (data.races.length > 0) {
+      const race = data.races[0]
+      expect(race).toMatchObject({
+        round: expect.any(Number),
+        date: expect.any(String),
+        time: expect.any(String),
+        url: expect.any(String),
+        raceId: expect.any(String),
+        raceName: expect.any(String),
+        circuit: expect.any(Object),
+        sprintQualyResults: expect.any(Array),
+      })
+    }
+  })
+
+  test("should return sprint qualy race data for a valid year and round, with limit and offset", async ({
+    request,
+  }) => {
+    const response = await request.get(
+      `/api/2024/5/sprint/qualy?limit=1&offset=1`
+    )
+    expect(response.ok()).toBeTruthy()
+    expect(response.status()).toBe(200)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      limit: expect.any(Number),
+      offset: expect.any(Number),
+      total: expect.any(Number),
+      season: expect.any(String),
+      races: expect.any(Object),
+    })
+
+    // Verificar una carrera dentro de la respuesta
+    if (data.races.length > 0) {
+      const race = data.races[0]
+      expect(race).toMatchObject({
+        round: expect.any(Number),
+        date: expect.any(String),
+        time: expect.any(String),
+        url: expect.any(String),
+        raceId: expect.any(String),
+        raceName: expect.any(String),
+        circuit: expect.any(Object),
+        sprintQualyResults: expect.any(Array),
+      })
+    }
+  })
+
+  test("should return 404 when no year are found", async ({ request }) => {
+    const response = await request.get(`/api/2099/1/sprint/qualy`)
+    expect(response.status()).toBe(404)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      message: expect.any(String),
+      status: 404,
+    })
+  })
+
+  test("should return 404 when no round are found", async ({ request }) => {
+    const response = await request.get(`/api/2024/30/sprint/qualy`)
+    expect(response.status()).toBe(404)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      message: expect.any(String),
+      status: 404,
+    })
+  })
+
+  test("should return 404 when no sprint qualy are found for this round", async ({
+    request,
+  }) => {
+    const response = await request.get(`/api/2024/1/sprint/qualy`)
+    expect(response.status()).toBe(404)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      message: expect.any(String),
+      status: 404,
+    })
+  })
+})
