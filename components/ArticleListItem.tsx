615249f47c4b6c9a8f12c9c3b3a9ed1acb53988b
Rafa <rafacv23@gmail.com>
2025-01-02 14:57:38 +0100

    refactor: simplify ArticleListItem component structure and improve card usage

--- components/ArticleListItem.tsx
+++ components/ArticleListItem.tsx
@@ -9,28 +9,27 @@ interface Props {
 
 const ArticleItemList = ({ articles }: Props) => {
   return (
-    <Card className="flex flex-col gap-5">
+    <>
       {articles.map((article, id) => (
-        <CardHeader
-          className="flex flex-col gap-2.5 font-poppins text-lg"
-          key={article.id}
-        >
-          <CardTitle>
-            <Link
-              href={`/blog/${article.id}`}
-              key={id}
-              className="hover:text-f1 transition duration-150"
-            >
-              {article.title}
-            </Link>
-          </CardTitle>
-          <CardDescription>
-            {moment(article.date, "DD-MM-YYYY").format("MMMM Do YYYY")} by{" "}
-            {article.author}
-          </CardDescription>
-        </CardHeader>
+        <Card key={article.id} className="my-4">
+          <CardHeader className="flex flex-col gap-2.5 font-poppins text-lg">
+            <CardTitle>
+              <Link
+                href={`/blog/${article.id}`}
+                key={id}
+                className="hover:text-f1 transition duration-150"
+              >
+                {article.title}
+              </Link>
+            </CardTitle>
+            <CardDescription>
+              {moment(article.date, "DD-MM-YYYY").format("MMMM Do YYYY")} by{" "}
+              {article.author}
+            </CardDescription>
+          </CardHeader>
+        </Card>
       ))}
-    </Card>
+    </>
   )
 }
 
