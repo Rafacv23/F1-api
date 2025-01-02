97f16bb1f9ae243f21875e65514365de6ea7500a
Rafa <rafacv23@gmail.com>
2025-01-02 14:34:24 +0100

    feat: adjust width of DocsNav component for improved responsiveness

--- components/navs/DocsNav.tsx
+++ components/navs/DocsNav.tsx
@@ -9,7 +9,7 @@ import {
 
 export default function DocsNav() {
   return (
-    <nav className="hidden md:block w-full md:min-w-fit md:max-w-xs border-r pr-4 border-headerBorder dark:border-customGrayDark sticky top-32 h-screen overflow-y-auto">
+    <nav className="hidden md:block w-full md:min-w-fit md:max-w-xs md:w-2/6 border-r pr-4 border-headerBorder dark:border-customGrayDark sticky top-32 h-screen overflow-y-auto">
       <Link
         href="/docs"
         className="font-semibold text-lg mb-4 hover:text-f1 hover:transition-colors"
