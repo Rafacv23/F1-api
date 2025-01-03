16b2feab70859555587d9fc3b62c3f3de04b22e0
Rafa <rafacv23@gmail.com>
2025-01-03 13:49:09 +0100

    test: add Playwright tests for current year API endpoints and data validation

---
+++ tests/api/current/[round]/route.spec.ts
@@ -0,0 +1,67 @@
+import { CURRENT_YEAR } from "@/lib/constants"
+import { test, expect } from "@playwright/test"
+
+test.describe("GET /api/current/[round]", async () => {
+  test("should return race data for a valid year and round", async ({
+    request,
+  }) => {
+    const response = await request.get(`/api/${CURRENT_YEAR}/3`)
+    expect(response.ok()).toBeTruthy()
+    expect(response.status()).toBe(200)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      total: expect.any(Number),
+      season: expect.any(String),
+      round: expect.any(String),
+      championship: expect.any(Object),
+      race: expect.any(Array),
+    })
+
+    // Verificar una carrera dentro de la respuesta
+    if (data.race.length > 0) {
+      const race = data.race[0]
+      expect(race).toMatchObject({
+        raceId: expect.any(String),
+        championshipId: expect.any(String),
+        raceName: expect.any(String),
+        schedule: expect.any(Object),
+        laps: expect.any(Number),
+        round: expect.any(Number),
+        fast_lap: expect.any(Object),
+        url: expect.any(String),
+        circuit: expect.any(Object),
+        winner: expect.any(Object),
+        teamWinner: expect.any(Object),
+      })
+    }
+  })
+
+  test("should return 404 when no year are found", async ({ request }) => {
+    const response = await request.get(`/api/2099/1`)
+    expect(response.status()).toBe(404)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      message: expect.any(String),
+      status: 404,
+    })
+  })
+
+  test("should return 404 when no round are found", async ({ request }) => {
+    const response = await request.get(`/api/${CURRENT_YEAR}/30`)
+    expect(response.status()).toBe(404)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      message: expect.any(String),
+      status: 404,
+    })
+  })
+})
---
+++ tests/api/current/drivers-championship.spec.ts
@@ -0,0 +1,59 @@
+import { CURRENT_YEAR } from "@/lib/constants"
+import { test, expect } from "@playwright/test"
+
+test.describe("GET /api/[year]/drivers-championship", async () => {
+  test("should return drivers championships by the current year", async ({
+    request,
+  }) => {
+    const response = await request.get(
+      `/api/${CURRENT_YEAR}/drivers-championship`
+    )
+    expect(response.ok()).toBeTruthy()
+    expect(response.status()).toBe(200)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      limit: expect.any(Number),
+      offset: expect.any(Number),
+      total: expect.any(Number),
+      season: expect.any(String),
+      championshipId: expect.any(String),
+      drivers_championship: expect.any(Array),
+    })
+
+    if (data.drivers_championship.length > 0) {
+      const driver = data.drivers_championship[0]
+      expect(driver).toMatchObject({
+        classificationId: expect.any(Number),
+        driverId: expect.any(String),
+        teamId: expect.any(String),
+        points: expect.any(Number),
+        position: expect.any(Number),
+        wins: expect.any(Number),
+        driver: expect.any(Object),
+        team: expect.any(Object),
+      })
+    }
+  })
+
+  test("should return 404 when no year are found", async ({ request }) => {
+    const response = await request.get(`/api/inventedYear/drivers-championship`)
+    expect(response.status()).toBe(404)
+
+    const data = await response.json()
+    expect(data).toMatchObject({
+      api: expect.any(String),
+      url: expect.any(String),
+      message: expect.any(String),
+      status: 404,
+    })
+  })
+
+  // test("should handle server errors gracefully", async ({ request }) => {
+  //   // Simula un caso donde el endpoint lanza un error, por ejemplo, manipulando datos inválidos o desconectando la DB
+  //   const response = await request.get(`/api/circuitsinvalida`)
+  //   expect(response.status()).toBe(500)
+  // })
+})
