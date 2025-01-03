e334667c455931161050a5a69f77a91a4365c224
Rafa <rafacv23@gmail.com>
2025-01-03 14:57:44 +0100

    fix: update site name to reflect API in constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,6 @@
 export const SITE_URL = "https://f1connectapi.vercel.app"
 export const SITE_URL_API = "https://f1connectapi.vercel.app/api/"
-export const SITE_NAME = "F1 Connect"
+export const SITE_NAME = "F1 Connect API"
 export const SITE_TITLE = "F1 Connect Api."
 export const SITE_META_NAME =
   "F1 Connect API | Free and open source F1 API that provides data about all Formula 1 Seasons."
