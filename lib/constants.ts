21000141bce3a3fbd20c9ee19224635db76a25ae
Rafa <rafacv23@gmail.com>
2025-01-22 23:55:36 +0100

    feat: update site constants to reflect new API URL and branding

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,9 +1,9 @@
-export const SITE_URL = "https://f1connectapi.vercel.app"
-export const SITE_URL_API = "https://f1connectapi.vercel.app/api/"
-export const SITE_NAME = "F1 Connect API"
-export const SITE_TITLE = "F1 Connect Api."
+export const SITE_URL = "https://f1api.dev"
+export const SITE_URL_API = "https://f1api.dev/api/"
+export const SITE_NAME = "F1 API"
+export const SITE_TITLE = "F1 Api."
 export const SITE_META_NAME =
-  "F1 Connect API | Free and open source F1 API that provides data about all Formula 1 Seasons."
+  "F1 API | Free and open source F1 API that provides data about all Formula 1 Seasons."
 export const SITE_META_DESCRIPTION =
-  "F1 Connect API it's a free and open source API that provides data about all Formula 1 Seasons. The API offers realtime data and historical."
+  "F1 API it's a free and open source API that provides data about all Formula 1 Seasons. The API offers realtime data and historical."
 export const CURRENT_YEAR = 2024 // Change this value every year, displaying the latest season with data.
