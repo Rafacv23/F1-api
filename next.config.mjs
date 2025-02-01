42c7abcab7a2cacc222a620dd0ab370535940a81
Rafa <rafacv23@gmail.com>
2025-02-01 10:48:09 +0100

    feat: remove Access-Control-Allow-Credentials header from API configuration

--- next.config.mjs
+++ next.config.mjs
@@ -6,10 +6,6 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           {
-            key: "Access-Control-Allow-Credentials",
-            value: "true",
-          },
-          {
             key: "Access-Control-Allow-Origin",
             value: "https://f1api.dev",
           },
