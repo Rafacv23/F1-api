3ff476493d972e449330f6becceb224a459083cc
Rafa Canosa <rafacv23@gmail.com>
2025-02-03 17:27:35 +0100

    feat: include subject in email sending functionality and update sender email format

--- lib/brevo.ts
+++ lib/brevo.ts
@@ -1,3 +1,5 @@
+"use server"
+
 import * as brevo from "@getbrevo/brevo"
 
 const apiInstance = new brevo.TransactionalEmailsApi()
@@ -9,19 +11,20 @@ apiInstance.setApiKey(
 
 // params received from the frontend form
 interface Params {
+  subject: string
   name: string
   email: string
   message: string
 }
 
-export async function sendEmail({ name, email, message }: Params) {
+export async function sendEmail({ subject, name, email, message }: Params) {
   try {
     const smtpEmail = new brevo.SendSmtpEmail()
 
-    //smtpEmail.subject = subject
+    smtpEmail.subject = subject
     smtpEmail.to = [{ email: "rafacv23@gmail.com" }]
-    smtpEmail.htmlContent = `<html><body>${message}</body></html>`
-    smtpEmail.sender = { name: name, email: email }
+    smtpEmail.htmlContent = `<html><body>${message} from ${email}</body></html>`
+    smtpEmail.sender = { name: name, email: "rafacv23@gmail.com" }
 
     await apiInstance.sendTransacEmail(smtpEmail)
   } catch (error) {
