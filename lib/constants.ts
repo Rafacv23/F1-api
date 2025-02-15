6331d98981e5e00a483dc9f914693ce1ad4aedfd
Rafa Canosa <rafacv23@gmail.com>
2025-02-15 02:05:33 +0100

    feat: enhance metadata and constants for improved SEO and branding

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,7 +2,8 @@ export const SITE_URL = "https://f1api.dev"
 export const SITE_URL_API = "https://f1api.dev/api/"
 export const SITE_NAME = "F1 API"
 export const SITE_TITLE = "F1 Api."
-export const SITE_META_NAME = "F1 API | Free and open source F1 API."
+export const SITE_META_NAME =
+  "Open F1 API - Live Formula 1 Data & Stats | Free & Open Source"
 export const GITHUB_REPO = "https://github.com/Rafacv23/F1-api"
 export const SITE_META_DESCRIPTION =
   "F1 API it's a free and open source API that provides data about all Formula 1 Seasons. The API offers realtime data and historical."
